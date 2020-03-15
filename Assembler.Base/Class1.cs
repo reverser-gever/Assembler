614ee59ce9bec797926658d560ec8dd66b3e286a
Bar <matoki.bar@gmail.com>
2020-03-15 23:11:23 +0200

    Add project files.

---
+++ Assembler.Base/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Assembler.Base
+{
+    public class Class1
+    {
+    }
+}
---
+++ Assembler.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Assembler.Core
+{
+    public class Class1
+    {
+    }
+}
