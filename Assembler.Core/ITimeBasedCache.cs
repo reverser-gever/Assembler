7a85f81b0761c45765e1b7d429a8a3c398d4e5e3
Bar <matoki.bar@gmail.com>
2020-03-16 11:07:21 +0200

    Added time based cache

---
+++ Assembler.Core/ITimeBasedCache.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using CacheManager.Core;
+
+namespace Assembler.Core
+{
+    public interface ITimeBasedCache<TIn, out TOut> : ICache<TIn>
+    {
+        event Action<TOut> ItemExpired;
+    }
+}
