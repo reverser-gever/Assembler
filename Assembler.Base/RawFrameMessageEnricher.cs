c1ee3db73650f3220dab670e7f96d9c13df25461
Bar <matoki.bar@gmail.com>
2020-03-18 17:21:59 +0200

    Added raw assembling imlementation

---
+++ Assembler.Base/RawFrameMessageEnricher.cs
@@ -0,0 +1,15 @@
+﻿using Assembler.Core;
+using Assembler.Core.Entities;
+using Assembler.Core.RawAssemblingEntities;
+
+namespace Assembler.Base
+{
+    public class RawFrameMessageEnricher : IMessageEnricher<BaseFrame, RawMessageInAssembly>
+    {
+        public void Enrich(BaseFrame frame, BaseMessageInAssembly message)
+        {
+            var rawMessage = message as RawMessageInAssembly;
+            rawMessage?.AssembledFrames.Add(frame);
+        }
+    }
+}
---
+++ Assembler.Core/RawAssemblingEntities/RawMessageInAssembly.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Assembler.Core.Entities;
+
+namespace Assembler.Core.RawAssemblingEntities
+{
+    public class RawMessageInAssembly : BaseMessageInAssembly
+    {
+        public List<BaseFrame> AssembledFrames { get; }
+
+        public RawMessageInAssembly()
+        {
+            AssembledFrames = new List<BaseFrame>();
+        }
+    }
+}
